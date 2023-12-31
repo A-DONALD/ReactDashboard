2b7a9690936c5f87cf8ebf5227f70fc3dd9f74c5
Y-DONALD <donaldyoungoua@gmail.com>
2023-12-31 18:58:46 +0100

    Add Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import DataBase from "./pages/DataBase"
+import NotFound from "./error/NotFound";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <DataBase /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "*", element: <NotFound /> }
+])
 
 function App() {
   return (
-    <p className="text-3xl font-bold underline">
-      Hello world!
-    </p>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
